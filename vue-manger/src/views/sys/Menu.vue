23001ab9633cd0e5f4414b12aeded4c54fb4a81e
202204170031 cjy <amianmmm@163.com>
2025-10-26 02:39:40 +0800

    第六章:进度2：完成菜单管理增删改功能

--- vue-manger/src/views/sys/Menu.vue
+++ vue-manger/src/views/sys/Menu.vue
@@ -72,10 +72,11 @@
           prop="Operate"
           label="操作">
         <template slot-scope="scope">
-          <el-button type="text">编辑</el-button>
+          <el-button type="text" @click="editHandle(scope.row.id)">编辑</el-button>
           <el-divider direction="vertical"></el-divider>
+
           <template>
-            <el-popconfirm title="确定删除吗">
+            <el-popconfirm @confirm="delHandle(scope.row.id)" title="确定删除吗">
               <el-button type="text" slot="reference">删除</el-button>
             </el-popconfirm>
           </template>
@@ -158,56 +159,26 @@
       return{
         dialogVisible:false,
         editForm:{
-          editFormRules: {
-            parentId: [
-              {required: true, message: '请选择上级菜单', trigger: 'blur'}
-            ],
-            name: [
-              {required: true, message: '请输入名称', trigger: 'blur'}
-            ],
-            perms: [
-              {required: true, message: '请输入权限编码', trigger: 'blur'}
-            ],
-            type: [
-              {required: true, message: '请选择类型', trigger: 'blur'}
-            ],
-            orderNum: [
-              {required: true, message: '请填入排序号', trigger: 'blur'}
-            ],
-            statu: [
-              {required: true, message: '请选择状态', trigger: 'blur'}
-            ]
-          },
-
-/////////对话框提交方法
-
-          submitForm(formName){
-            this.$refs[formName].validate((valid) => {
-              if (valid) {
-                this.$axios.post('/sys/menu/'+(this.editForm.id?'update':'save'), this.editForm)
-                    .then(res => {
-                      this.$message({
-                        showClose: true,
-                        message: '恭喜，操作成功',
-                        type: 'success',
-                        onClose:()=>{
-                          this.getMenuTree()
-                        }
-                      });
-                      this.resetForm('editForm')
-
-                    })
-                    .catch(error => {
-                      console.error('操作失败:', error)
-                    })
-
-              }
-              else {
-                console.log('error submit!!');
-                return false;
-              }
-            });
-          },
+        },
+        editFormRules: {
+          parentId: [
+            {required: true, message: '请选择上级菜单', trigger: 'blur'}
+          ],
+          name: [
+            {required: true, message: '请输入名称', trigger: 'blur'}
+          ],
+          perms: [
+            {required: true, message: '请输入权限编码', trigger: 'blur'}
+          ],
+          type: [
+            {required: true, message: '请选择类型', trigger: 'blur'}
+          ],
+          orderNum: [
+            {required: true, message: '请填入排序号', trigger: 'blur'}
+          ],
+          statu: [
+            {required: true, message: '请选择状态', trigger: 'blur'}
+          ]
         },
         tableData: []
 
@@ -221,7 +192,62 @@
         this.$axios.get('/sys/menu/list').then(res => {
           this.tableData = res.data.data;
         })
+      },
+
+      submitForm(formName){
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            this.$axios.post('/sys/menu/'+(this.editForm.id?'update':'save'), this.editForm)
+                .then(res => {
+                  this.$message({
+                    showClose: true,
+                    message: '恭喜，操作成功',
+                    type: 'success',
+                    onClose:()=>{
+                      this.getMenuTree()
+                    }
+                  });
+                  this.resetForm('editForm')
+
+                })
+                .catch(error => {
+                  console.error('操作失败:', error)
+                })
+
+          }
+          else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      editHandle(id){
+        this.$axios.get("/sys/menu/info/" + id).then(res => {
+          this.editForm = res.data.data
+          this.dialogVisible = true
+        })
+      },
+      resetForm(formName){
+        this.$refs[formName].resetFields();
+        this.dialogVisible = false
+        this.editForm = {}
+      },
+      handleClose(){
+        this.resetForm('editForm')
+      },
+      delHandle(id){
+        this.$axios.post("/sys/menu/delete/" + id).then(res => {
+          this.$message({
+            showClose: true,
+            message: '删除成功',
+            type: 'success',
+            onClose:()=>{
+              this.getMenuTree()
+            }
+          });
+        })
       }
+
     }
   }
 </script>
