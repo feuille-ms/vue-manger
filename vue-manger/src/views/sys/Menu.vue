26bb2efc4871a2e69102e576d8aa0befdd422dd9
202204170031 cjy <amianmmm@163.com>
2025-11-06 23:04:23 +0800

    第七章：进度1：完成角色功能界面，包括新增，编辑，单个删除，批量删除(前端)。

--- vue-manger/src/views/sys/Menu.vue
+++ vue-manger/src/views/sys/Menu.vue
@@ -20,7 +20,7 @@
           width="180">
       </el-table-column>
       <el-table-column
-          prop="pers"
+          prop="perms"
           label="权限编码"
           sortable
           width="180">
@@ -83,6 +83,8 @@
         </template>
       </el-table-column>
     </el-table>
+
+
     <el-dialog
         title="提示"
         :visible.sync="dialogVisible"
